40e0d314db869fd268576bc017a1ea7e8423731b
Me <shalygina-tatyana@mail.ru>
2016-06-08 16:19:25 +0500

    2 commit

--- pyatnashki.cpp
+++ pyatnashki.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+int field[4][4];
+
+int EmptyX, EmptyY;
+
+void CreateField()
+{
+	int Nums[15]; 					// массив кост€шек
+ 	int RandNum; 
+ 	for (int i = 0; i < 15; i++) 	// генерируем кост€шки
+ 	{
+  		RandNum = (rand() %15) + 1; 
+  		Nums[i] = RandNum; 
+ 	}
+}
+
 main ()
 {
-	cout << "Hello worlddd!";
+	CreateField ();
+	
 }
